61c561b2412f5688b941ad6df2d7793ab335cbe0
mohammadtavassolian3 <mohammadtavassolian3@gmail.com>
2025-05-23 17:45:09 +0330

    database connection is completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,10 @@ import { AppConfigModule } from './config/app/app-config.module';
 import { DatabaseConfigModule } from './config/database/database-config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfigService } from './config/database/database-config.service';
+import { DatabaseModule } from './config/database/database.module';
 
 @Module({
-  imports: [AppConfigModule, TypeOrmModule.forRootAsync({
-    imports: [DatabaseConfigModule],
-    inject: [DatabaseConfigService],
-    useFactory: (dbConfig: DatabaseConfigService) => ({
-      type: dbConfig.type as any,
-      host: dbConfig.host,
-      port: dbConfig.port,
-      username: dbConfig.username,
-      password: dbConfig.password,
-    })
-  })],
+  imports: [AppConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database/database.module.ts
@@ -0,0 +1,20 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DatabaseConfigModule } from "./database-config.module";
+import { DatabaseConfigService } from "./database-config.service";
+
+@Module({
+    imports: [TypeOrmModule.forRootAsync({
+        imports: [DatabaseConfigModule],
+        inject: [DatabaseConfigService],
+        useFactory: (dbConfig: DatabaseConfigService) => ({
+            type: dbConfig.type as any,
+            host: dbConfig.host,
+            port: dbConfig.port,
+            username: dbConfig.username,
+            password: dbConfig.password,
+            synchronize: dbConfig.synchronize
+        })
+    })],
+})
+export class DatabaseModule { }
