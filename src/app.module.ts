e73c760ea46ae04bc5a62dfce30fb0b2698981be
baradd <mohammadtavassolian3@gmail.com>
2025-07-01 17:10:06 +0330

    developing

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
+      rootPath: join(__dirname, 'public'),
       serveRoot: '/static',
     }),
     AppConfigModule,
